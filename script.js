db00cf473f141d156ca7aa820b644ad238791bdd
Grahambone37 <electricgrunt37@gmail.com>
2022-12-03 10:54:02 -0800

    set up fishing zone

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function main(){
-/*-----reference for howtosun----
-    let topSun = document.querySelector('.top');
-    topSun.style.backgroundColor = 'yellow'
-*/
+function main() {
+    /*-----reference for backgroundchanges----
+        let topSun = document.querySelector('.top');
+        topSun.style.backgroundColor = 'yellow'
+        let fishZone = document.querySelector('#fishing-zone');
+        fishZone.style.backgroundColor = 'rgb(0, 90, 92)'
+    */
 
-//add a sleep function
+    //add a sleep function
 
-//function smallfishencounter
+    //function smallfishencounter
 
-//mediumfishencounter
+    //mediumfishencounter
 
-//bigfishencounter
+    //bigfishencounter
 
-//function buttonpress = pick 1 of 3(eventually 4) of the fishencounters
+    //function buttonpress = pick 1 of 3(eventually 4) of the fishencounters
 
-//playbuttonpress---sequence of 10-random-fish, then game ends
+    //playbuttonpress---sequence of 10-random-fish, then game ends
 }
 main()
