18710131573adb87838ce13a30413f0e1538009a
Grahambone37 <electricgrunt37@gmail.com>
2022-12-03 09:54:52 -0800

    set up comments to prep functions

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function main(){
+/*-----reference for howtosun----
     let topSun = document.querySelector('.top');
     topSun.style.backgroundColor = 'yellow'
+*/
+
+//add a sleep function
+
+//function smallfishencounter
+
+//mediumfishencounter
+
+//bigfishencounter
+
+//function buttonpress = pick 1 of 3(eventually 4) of the fishencounters
+
+//playbuttonpress---sequence of 10-random-fish, then game ends
 }
 main()
